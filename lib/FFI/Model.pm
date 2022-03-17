4976da45f63c7f2eefb03498108ca887c278234e
Graham Ollis <plicease@cpan.org>
2022-03-17 11:51:54 -0600

    initial implementation

--- lib/FFI/Model.pm
+++ lib/FFI/Model.pm
@@ -2,10 +2,19 @@ package FFI::Model;
 
 use strict;
 use warnings;
-use 5.008004;
+use 5.032;
 
 # ABSTRACT: Model for FFI generated code
 # VERSION
+
+package FFI::Model::Function;
+package FFI::Model::Type;
+package FFI::Model::Constant;
+package FFI::Model::Enum;
+package FFI::Model::Record;
+package FFI::Model::Struct;
+package FFI::Model::Union;
+package FFI::Model::ArrayStruct;
 
 1;
 
